8c53db062cfb6e00be68abc93974c75292ddc251
LuisPozo-06 <luispg132@gmail.com>
2025-08-17 14:59:11 -0500

    Configurando appDbContext y cadena de conexcion

---
+++ backend.IdatBank/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using backend.IdatBank.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace backend.IdatBank.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios => Set<Usuario>();
+        public DbSet<Cuenta> Cuentas => Set<Cuenta>();
+        public DbSet<Transferencia> Transferencias => Set<Transferencia>();
+    }
+}
